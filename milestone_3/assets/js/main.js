9cbe2b2749ec1937cede6dedeaf1e0734938b1e8
Vituz <vtbattaglia@gmail.com>
2021-05-25 20:14:55 +0200

    Complete Milestone 5

--- milestone_3/assets/js/main.js
+++ milestone_3/assets/js/main.js
@@ -13,6 +13,10 @@ const app = new Vue({
 
         searchContact: null,
 
+        messageCounter: 0,
+
+        // messageMenu: true,
+
         user:{
             name: 'Vituz',
             avatar: '_io',
@@ -28,16 +32,19 @@ const app = new Vue({
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
                         status: 'sent',
+                        menu: true,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: true,
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        menu: true,
                     }
                 ],
             },
@@ -49,17 +56,20 @@ const app = new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: true,
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        menu: true,
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: true,
                     }
                 ],
             },
@@ -72,17 +82,20 @@ const app = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        menu: true,
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: true,
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        menu: true,
                     }
                 ],
             },
@@ -94,12 +107,14 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        menu: true,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        menu: true,
                     }
                 ],
             },
@@ -123,7 +138,8 @@ const app = new Vue({
             this.contacts[this.contactCounter].messages.push({
                 date: this.showDate,
                 text: this.message,
-                status: 'sent'
+                status: 'sent',
+                menu: true,
             });
 
             setTimeout(()=>{
@@ -135,24 +151,42 @@ const app = new Vue({
 
         },
 
+        deleteMessage(index){
+            this.contacts[this.contactCounter]
+            .messages.splice(index, 1);
+        },
+
         sendResponse(){
 
             this.contacts[this.contactCounter].messages.push({
                 date: this.showDate,
                 text: 'Ok!',
                 status: 'received',
+                menu: true,
             });
 
             // this.lastAccess = this.showDate;
             // console.log("ultimo accesso" + this.lastAccess);
         },
 
+        openMessageMenu(index){
+
+            // this.messageCounter = this.contacts[this.contactCounter]
+            // .messages[index];
+
+            console.log(this.messageCounter);
+            let message = this.contacts[this.contactCounter].messages[index].menu;
+            
+            if(message){
+                return this.contacts[this.contactCounter].messages[index].menu = false;
+            } else {
+                return this.contacts[this.contactCounter].messages[index].menu = true;
+            }
+        },
+
         findContacts(nomeContatto){
             return nomeContatto = this.searchContact;
         }
-
-        
-
     },
 
     mounted(){
